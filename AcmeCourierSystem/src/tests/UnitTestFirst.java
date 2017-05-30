d6bc37197cdd64077d12dfdd80ce07ef1e60a3ab
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2017-05-29 20:48:48 -0500

    added text

--- AcmeCourierSystem/src/tests/UnitTestFirst.java
+++ AcmeCourierSystem/src/tests/UnitTestFirst.java
@@ -4,7 +4,7 @@ public class UnitTestFirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Successfully committed.");
+		System.out.println("Successfully committed and changed.");
 	}
 
 }
