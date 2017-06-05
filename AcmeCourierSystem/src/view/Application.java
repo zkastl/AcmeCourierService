951b3939b55a84fc5d8ddc531d55ed48fc1428ed
Zachary Kastl <zkastl8@gmail.com>
2017-06-05 17:36:27 -0500

    Added basic saving/loading capability.

--- AcmeCourierSystem/src/view/Application.java
+++ AcmeCourierSystem/src/view/Application.java
@@ -1,14 +1,23 @@
 package view;
 
 import java.awt.Toolkit;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 
+import model.Employee;
+import model.Serialize;
+
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 
 		// TODO load data
+		Employee e = new Employee("Zak", 0002, "King");
+		Serialize.SaveObject("test.xml", e);
+		Employee e2 = (Employee)Serialize.LoadObject("test.xml", Employee.class);
+		System.out.println(e2.getName());
 
 		final JFrame window = new JFrame();
 		window.setContentPane(new LogInScreen(window));
