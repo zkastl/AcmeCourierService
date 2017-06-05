6a1f29959ec6cca6bac5c508e02e0c02c4f2cb67
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2017-06-05 17:33:45 -0500

    make tabs taller

--- AcmeCourierSystem/src/view/MainScreen.java
+++ AcmeCourierSystem/src/view/MainScreen.java
@@ -2,22 +2,39 @@ package view;
 
 import java.awt.Container;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
 public class MainScreen extends Container {
 	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
+	private JFrame window;
 
 	public MainScreen(JFrame window) {
+		this.window = window;
 		tabbedPane.setBounds(0, 0, window.getWidth(), window.getHeight());
 		add(tabbedPane);
-		tabbedPane.addTab("Ticket Management", new LogOutScreen(window));
-		tabbedPane.addTab("Client Management", new LogOutScreen(window));
-		tabbedPane.addTab("Report Management", new LogOutScreen(window));
-		tabbedPane.addTab("Map Management", new LogOutScreen(window));
-		tabbedPane.addTab("Employee Management", new LogOutScreen(window));
-		tabbedPane.addTab("System Settings", new LogOutScreen(window));
-		tabbedPane.addTab("Change Password", new LogOutScreen(window));
-		tabbedPane.addTab("Logout", new LogOutScreen(window));
+		tabbedPane.addTab("Ticket Management", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("Client Management", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("Report Management", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("Map Management", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("Employee Management", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("System Settings", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("Change Password", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+		tabbedPane.addTab("Logout", new ImageIcon(MainScreen.class.getResource("/view/taller tab.png")),
+				new LogOutScreen(window));
+	}
+
+	@Override
+	public void doLayout() {
+		tabbedPane.setBounds(0, 0, window.getWidth(), window.getHeight());
+		super.doLayout();
 	}
 }
