447b16d49f964a994a5631e22edf82a33157ab26
Zachary Kastl <zkastl8@gmail.com>
2017-06-05 19:29:07 -0500

    Made saving/loading not private

--- AcmeCourierSystem/src/model/CourierSystem.java
+++ AcmeCourierSystem/src/model/CourierSystem.java
@@ -9,18 +9,33 @@ public final class CourierSystem {
 	public ArrayList<Employee> Employees;
 	public ArrayList<Client> Clients;
 	public ArrayList<Delivery> Deliveries;
+	public Map CityMap;
 	public Settings SystemSettings;
-	
-	@SuppressWarnings("unused")
-	private static String settingsFile = "settings.xml";
 	
 	public CourierSystem() throws FileNotFoundException, IOException {
 		LoadEmployees();
-		
+		LoadCouriers();
+		LoadClients();
+		LoadDeliveries();
 	}
 	
+	public void LoadDeliveries() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void LoadClients() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void LoadCouriers() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@SuppressWarnings("unchecked")
-	private void LoadEmployees() throws FileNotFoundException, IOException {
+	public void LoadEmployees() throws FileNotFoundException, IOException {
 		try {
 			Employees = (ArrayList<Employee>)Serialize.LoadObject(Settings.EmployeeFile);
 		}
@@ -30,7 +45,7 @@ public final class CourierSystem {
 		}
 	}
 	
-	private void SaveEmployees() throws FileNotFoundException, IOException {
+	public void SaveEmployees() throws FileNotFoundException, IOException {
 		
 		Serialize.SaveObject(Settings.EmployeeFile, Employees);
 	}
