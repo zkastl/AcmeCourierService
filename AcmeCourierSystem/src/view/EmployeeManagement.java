066955e74a4e36b4368f5a56f3bbff5065abb0ac
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2017-06-07 20:50:04 -0500

    can now add employees and save changes.
    need to figure out how to allow edit of role from this view. also need
    to implement delete

--- AcmeCourierSystem/src/view/EmployeeManagement.java
+++ AcmeCourierSystem/src/view/EmployeeManagement.java
@@ -4,12 +4,14 @@ import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import controller.EnterKeyListenerForButtons;
 import model.CourierSystem;
 import model.Employee;
 import net.miginfocom.swing.MigLayout;
@@ -20,10 +22,10 @@ public class EmployeeManagement extends Container {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTable table;
+	EmployeeTableModel employeeTable;
 
 	public EmployeeManagement(CourierSystem data) {
-
-		EmployeeTableModel employeeTable = new EmployeeTableModel(data.Employees);
+		employeeTable = new EmployeeTableModel(data.Employees);
 		setLayout(new MigLayout("", "[grow][50%][grow][10]", "[25][40][5][grow][][20]"));
 
 		JLabel lblEmployeeManagement = new JLabel("Employee Management");
@@ -32,10 +34,16 @@ public class EmployeeManagement extends Container {
 
 		JButton btnAddEmployee = new JButton("Add Employee");
 		add(btnAddEmployee, "cell 0 1,alignx center,aligny bottom");
+		btnAddEmployee.addKeyListener(new EnterKeyListenerForButtons(btnAddEmployee));
+
+		JButton btnSaveChanges = new JButton("Save Changes");
+		add(btnSaveChanges, "cell 2 1");
+		btnSaveChanges.addKeyListener(new EnterKeyListenerForButtons(btnSaveChanges));
 
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane, "cell 0 3 3 1,grow");
 		table = new JTable(employeeTable);
+		table.setCellSelectionEnabled(true);
 		scrollPane.setViewportView(table);
 		table.setColumnSelectionAllowed(true);
 
@@ -44,8 +52,17 @@ public class EmployeeManagement extends Container {
 
 		btnAddEmployee.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				data.Employees.add(new Employee());
-				scrollPane.setViewportView(new JTable(new EmployeeTableModel(data.Employees)));
+				employeeTable.addRow(new Employee());
+			}
+		});
+
+		btnSaveChanges.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					data.SaveEmployees();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
 	}
--- AcmeCourierSystem/src/view/EmployeeTableModel.java
+++ AcmeCourierSystem/src/view/EmployeeTableModel.java
@@ -2,55 +2,41 @@ package view;
 
 import java.util.ArrayList;
 
-import javax.swing.event.TableModelListener;
-import javax.swing.table.TableModel;
+import javax.swing.table.DefaultTableModel;
 
 import model.Employee;
 import model.EmployeeRole;
 
-public class EmployeeTableModel implements TableModel {
+public class EmployeeTableModel extends DefaultTableModel {
 
 	private ArrayList<Employee> employees;
 
 	public EmployeeTableModel(ArrayList<Employee> employees) {
+		super(new Object[] { "ID", "Name", "Role", "User Name" }, 0);
 		this.employees = employees;
-	}
-
-	@Override
-	public int getRowCount() {
-		return employees.size();
-	}
-
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 4;
-	}
-
-	@Override
-	public String getColumnName(int columnIndex) {
-		// TODO Auto-generated method stub
-		switch (columnIndex) {
-		case 0:
-			return "ID";
-		case 1:
-			return "Name";
-		case 2:
-			return "Role";
-		default:
-			return null;
+		for (Employee e : employees) {
+			super.addRow(new Object[] { e.employeeID, e.name, e.role, e.userName });
 		}
 	}
 
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		// TODO Auto-generated method stub
-		return String.class;
+		switch (columnIndex) {
+		case 0:
+			return int.class;
+		case 1:
+			return String.class;
+		case 2:
+			return EmployeeRole.class;
+		case 3:
+			return String.class;
+		default:
+			return String.class;
+		}
 	}
 
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
 		switch (columnIndex) {
 		case 0:
 			return false;
@@ -61,7 +47,6 @@ public class EmployeeTableModel implements TableModel {
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
 		switch (columnIndex) {
 		case 0:
 			return employees.get(rowIndex).employeeID;
@@ -69,6 +54,8 @@ public class EmployeeTableModel implements TableModel {
 			return employees.get(rowIndex).name;
 		case 2:
 			return employees.get(rowIndex).role;
+		case 3:
+			return employees.get(rowIndex).userName;
 		default:
 			return null;
 		}
@@ -76,27 +63,23 @@ public class EmployeeTableModel implements TableModel {
 
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
 		switch (columnIndex) {
 		case 1:
 			employees.get(rowIndex).name = aValue.toString();
+			break;
 		case 2:
-			employees.get(rowIndex).role = (EmployeeRole) aValue;
+			employees.get(rowIndex).role = EmployeeRole.valueOf(aValue.toString());
+			break;
+		case 3:
+			employees.get(rowIndex).userName = aValue.toString();
 		default:
 			break;
 		}
 	}
 
-	@Override
-	public void addTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void removeTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-
+	public void addRow(Employee employee) {
+		super.addRow(new Object[] { employee.employeeID, employee.name, employee.role, employee.userName });
+		employees.add(employee);
 	}
 
 }
