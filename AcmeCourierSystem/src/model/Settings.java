d2629a530b516531fd8b7fee55ee7d8245aa1bb1
n1becker <n1becker@n1becker-PC>
2017-06-07 19:48:32 -0500

    Initial pathing

--- AcmeCourierSystem/src/model/Settings.java
+++ AcmeCourierSystem/src/model/Settings.java
@@ -9,44 +9,44 @@ public class Settings {
 	/**
 	 * The base price for a delivery
 	 */
-	public double baseCost;
+	public static double baseCost;
 	
 	/**
 	 * The price per block traveled that is added to the base price for a
 	 * delivery
 	 */
-	public double pricePerBlock;
+	public static double pricePerBlock;
 	
 	/**
 	 * The amount of non-travel time for pickup and delivery that is planned
 	 * into a delivery
 	 */
-	public double plannedNonTravelTime;
+	public static double plannedNonTravelTime;
 	
 	/**
 	 * The intersection of the company that couriers depart from and return to.
 	 */
-	public Intersection courierStartAddress;
+	public static Intersection courierStartAddress;
 	
 	/**
 	 * The average speed at which a courier travels. Used in delivery time
 	 * calculations
 	 */
-	public double averageCourierSpeed;
+	public static double averageCourierSpeed;
 	
 	/**
 	 * The time in minutes before and after the requested pickup and estimated
 	 * delivery times during which a delivery is considered on time. Default is
 	 * 5minutes.
 	 */
-	public int bonusLeeway;
+	public static int bonusLeeway;
 	
-	public double bonusAmount;
+	public static double bonusAmount;
 	
 	/**
 	 * the id that will be assigned to the next employee, client, or package
 	 */
-	public int nextID;
+	public static int nextID;
 	
 	// Settings to point to the locations for the persistent data.
 	public static String EmployeeFile = "Employees.xml";
