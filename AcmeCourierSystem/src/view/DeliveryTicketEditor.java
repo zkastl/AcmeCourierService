5199da3ceb397b4c5b5a76495f6c07716717ddff
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2017-06-19 21:15:55 -0500

    starting to add initial states and editable fields

--- AcmeCourierSystem/src/view/DeliveryTicketEditor.java
+++ AcmeCourierSystem/src/view/DeliveryTicketEditor.java
@@ -2,141 +2,205 @@ package view;
 
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 
+import com.github.lgooddatepicker.components.DateTimePicker;
+
 import main.Application;
+import main.CourierSystem;
 import model.Delivery;
-import net.miginfocom.swing.MigLayout;
+import net.miginfocom.swing.MigLayout;;
 
 public class DeliveryTicketEditor extends JDialog {
 
 	public DeliveryTicketEditor(Delivery delivery) {
 		super((JFrame) null, "ACME Delivery Ticket Editor", true);
 
+		setSize(650, 700);
+		setAlwaysOnTop(true);
+
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Application.class.getResource("/view/courier logo.png")));
 		setTitle("ACME Delivery Ticket Editor");
-		setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+		setResizable(false);
 
-		setVisible(true);
+		getContentPane().setLayout(new MigLayout("", "[grow][20%][20%,grow][20%,grow][20%][grow]",
+				"[][][5:n][][][20:n][][][][][][20:n][][][][][20:n][][][][][][][20][]"));
 
-		setLayout(new MigLayout("", "[25%][25%][25%][25%]", "[][][][][][][][][][][][][][][][][][][][][][]"));
-		JLabel lblAcmeCourierService = new JLabel("");
-		lblAcmeCourierService.setIcon(new ImageIcon(DeliveryTicketEditor.class.getResource("/view/courier logo.png")));
-		add(lblAcmeCourierService, "cell 0 0 4 1,alignx center");
+		JLabel logo = new JLabel("");
+		logo.setIcon(new ImageIcon(DeliveryTicketEditor.class.getResource("/view/courier logo.png")));
+		getContentPane().add(logo, "cell 1 0 4 1,alignx center");
 
 		JLabel lblAcmeCourierService_1 = new JLabel("ACME Courier Service");
 		lblAcmeCourierService_1.setIcon(null);
 		lblAcmeCourierService_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		add(lblAcmeCourierService_1, "cell 0 1 4 1,alignx center,aligny center");
+		getContentPane().add(lblAcmeCourierService_1, "flowy,cell 1 1 4 1,alignx center,aligny center");
+
+		JLabel lblDate = new JLabel("Date:");
+		getContentPane().add(lblDate, "flowx,cell 2 3 2 1,alignx center");
+
+		JLabel lblOrderTakenBy = new JLabel("Order taken by:");
+		getContentPane().add(lblOrderTakenBy, "flowx,cell 2 4 2 1,alignx center");
+
+		JLabel lblPickUpInfo = new JLabel("Pick Up Info");
+		lblPickUpInfo.setIcon(null);
+		lblPickUpInfo.setFont(new Font("Tahoma", Font.BOLD, 12));
+		getContentPane().add(lblPickUpInfo, "cell 2 6 2 1,alignx center");
+
+		JLabel lblClientName = new JLabel("Client Name:");
+		lblClientName.setIcon(null);
+		getContentPane().add(lblClientName, "cell 2 7,alignx trailing");
+
+		// TODO
+		JComboBox pickupClient = new JComboBox();
+		getContentPane().add(pickupClient, "cell 3 7,growx");
+
+		JLabel lblClientId = new JLabel("Client Id:");
+		lblClientId.setIcon(null);
+		getContentPane().add(lblClientId, "cell 2 8,alignx trailing");
+
+		JLabel pickupClientId = new JLabel("");
+		getContentPane().add(pickupClientId, "flowx,cell 3 8");
+
+		JLabel lblRequestedPickup = new JLabel("Requested Pickup Time:");
+		getContentPane().add(lblRequestedPickup, "cell 2 9,alignx trailing");
+
+		// TODO: validation
+		DateTimePicker pickupEditor = new DateTimePicker();
+		getContentPane().add(pickupEditor, "cell 3 9 2 1,alignx left");
+
+		JLabel lblBillPickupClient = new JLabel("Bill Pickup Client?");
+		getContentPane().add(lblBillPickupClient, "cell 2 10,alignx trailing");
+
+		JRadioButton billToPickup = new JRadioButton("");
+		billToPickup.setSelected(true);
+		getContentPane().add(billToPickup, "cell 3 10");
+
+		JLabel lblDeliveryInfo = new JLabel("Delivery Info");
+		lblDeliveryInfo.setFont(new Font("Tahoma", Font.BOLD, 12));
+		getContentPane().add(lblDeliveryInfo, "cell 2 12 2 1,alignx center");
+
+		JLabel lblClientName_1 = new JLabel("Client Name:");
+		getContentPane().add(lblClientName_1, "cell 2 13,alignx trailing");
+
+		// TODO
+		JComboBox comboBox = new JComboBox();
+		getContentPane().add(comboBox, "cell 3 13,growx");
+
+		JLabel lblClientId_1 = new JLabel("Client Id:");
+		getContentPane().add(lblClientId_1, "flowx,cell 2 14,alignx trailing");
+
+		JLabel lblDeliveryclient = new JLabel("");
+		getContentPane().add(lblDeliveryclient, "cell 3 14,alignx center");
+
+		JLabel lblBillDeliveryClient = new JLabel("Bill Delivery Client?");
+		getContentPane().add(lblBillDeliveryClient, "cell 2 15,alignx trailing");
+
+		JRadioButton billToDeliver = new JRadioButton("");
+		getContentPane().add(billToDeliver, "cell 3 15");
+
+		ButtonGroup radioGroup = new ButtonGroup();
+		radioGroup.add(billToPickup);
+		radioGroup.add(billToDeliver);
+
+		JLabel lblNewLabel = new JLabel("Ticket Info");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		getContentPane().add(lblNewLabel, "cell 1 17 2 1,alignx center");
+
+		JLabel lblDeliveryInfo_1 = new JLabel("Delivery Info");
+		lblDeliveryInfo_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		getContentPane().add(lblDeliveryInfo_1, "cell 3 17 2 1,alignx center");
+
+		JLabel lblPackageId = new JLabel("Package Id:");
+		getContentPane().add(lblPackageId, "cell 1 18,alignx trailing");
+
+		JLabel packageId = new JLabel("");
+		getContentPane().add(packageId, "cell 2 18");
+
+		JLabel lblCourier = new JLabel("Courier:");
+		getContentPane().add(lblCourier, "cell 3 18,alignx trailing");
+
+		// TODO
+		JComboBox cbCourier = new JComboBox();
+		getContentPane().add(cbCourier, "cell 4 18,growx");
+
+		JLabel lblEstimatedDeliveryTime = new JLabel("Estimated Delivery Time:");
+		getContentPane().add(lblEstimatedDeliveryTime, "cell 1 19,alignx trailing");
+
+		JLabel estimatedDelivery = new JLabel("");
+		getContentPane().add(estimatedDelivery, "cell 2 19");
+
+		JLabel lblAssignedTime = new JLabel("Assigned Time:");
+		getContentPane().add(lblAssignedTime, "cell 3 19,alignx trailing");
+
+		JLabel departureTime = new JLabel("");
+		getContentPane().add(departureTime, "cell 4 19");
+
+		JLabel lblEstimatedBlocks = new JLabel("Estimated Blocks:");
+		getContentPane().add(lblEstimatedBlocks, "cell 1 20,alignx trailing");
+
+		JLabel blocks = new JLabel("");
+		getContentPane().add(blocks, "cell 2 20");
+
+		JLabel lblPickedUpTime = new JLabel("Picked Up Time:");
+		getContentPane().add(lblPickedUpTime, "cell 3 20,alignx trailing");
+
+		JLabel lblQuotedPrice = new JLabel("Quoted Price:");
+		getContentPane().add(lblQuotedPrice, "cell 1 21,alignx trailing");
+
+		JLabel price = new JLabel("");
+		getContentPane().add(price, "cell 2 21");
+
+		JLabel lblDeliveredTime = new JLabel("Delivered Time?");
+		getContentPane().add(lblDeliveredTime, "cell 3 21,alignx trailing");
+
+		JLabel lblBonus = new JLabel("Bonus?");
+		getContentPane().add(lblBonus, "cell 3 22,alignx trailing");
+
+		// TODO: Disable without looking disabled
+		JCheckBox bonusEarned = new JCheckBox("");
+		getContentPane().add(bonusEarned, "cell 4 22");
+
+		// TODO
+		JButton btnSave = new JButton("Ok");
+		getContentPane().add(btnSave, "cell 0 24,growx");
+
+		// TODO
+		JButton btnApply = new JButton("Apply");
+		getContentPane().add(btnApply, "flowx,cell 1 24,alignx center");
+
+		// TODO
+		JButton btnCancel = new JButton("Cancel");
+		getContentPane().add(btnCancel, "cell 1 24");
+
+		JLabel lblDateValue = new JLabel(LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+		getContentPane().add(lblDateValue, "cell 2 3 2 1");
+
+		JLabel lblTime = new JLabel("Time:");
+		getContentPane().add(lblTime, "cell 2 3 2 1");
+
+		JLabel lblTimeValue = new JLabel(LocalTime.now().format(DateTimeFormatter.ofPattern("h:mm a")));
+		getContentPane().add(lblTimeValue, "cell 2 3 2 1");
+
+		JLabel lblOrdertaker = new JLabel(CourierSystem.currentUser.name);
+		getContentPane().add(lblOrdertaker, "cell 2 4 2 1");
 
 		JLabel lblDeliveryTicket = new JLabel("Delivery Ticket");
 		lblDeliveryTicket.setIcon(null);
 		lblDeliveryTicket.setFont(new Font("Tahoma", Font.BOLD, 14));
-		add(lblDeliveryTicket, "cell 0 2 4 1,alignx center,aligny center");
+		getContentPane().add(lblDeliveryTicket, "cell 1 1 4 1,alignx center,aligny center");
 
-		JLabel lblPickUpInfo = new JLabel("Pick Up Info");
-		lblPickUpInfo.setIcon(null);
-		lblPickUpInfo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		add(lblPickUpInfo, "cell 1 4,alignx trailing");
-
-		JLabel lblClientName = new JLabel("Client Name:");
-		lblClientName.setIcon(null);
-		add(lblClientName, "cell 1 5,alignx trailing");
-
-		JComboBox pickupClient = new JComboBox();
-		add(pickupClient, "cell 2 5,growx");
-
-		JLabel lblClientId = new JLabel("Client Id:");
-		lblClientId.setIcon(null);
-		add(lblClientId, "cell 1 6,alignx trailing");
-
-		JLabel pickupClientId = new JLabel("");
-		add(pickupClientId, "cell 2 6");
-
-		JLabel lblRequestedPickup = new JLabel("Requested Pickup Time:");
-		add(lblRequestedPickup, "cell 1 7,alignx trailing");
-
-		JLabel lblBillPickupClient = new JLabel("Bill Pickup Client?");
-		add(lblBillPickupClient, "cell 1 8,alignx trailing");
-
-		JRadioButton radioButton = new JRadioButton("");
-		radioButton.setSelected(true);
-		add(radioButton, "cell 2 8");
-
-		JLabel lblDeliveryInfo = new JLabel("Delivery Info");
-		lblDeliveryInfo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		add(lblDeliveryInfo, "cell 1 10,alignx center");
-
-		JLabel lblClientName_1 = new JLabel("Client Name:");
-		add(lblClientName_1, "cell 1 11,alignx trailing");
-
-		JLabel lblClientId_1 = new JLabel("Client Id:");
-		add(lblClientId_1, "flowx,cell 1 12,alignx trailing");
-
-		JLabel lblBillDeliveryClient = new JLabel("Bill Delivery Client?");
-		add(lblBillDeliveryClient, "cell 1 13,alignx trailing");
-
-		JRadioButton radioButton_1 = new JRadioButton("");
-		add(radioButton_1, "cell 2 13");
-
-		JLabel lblNewLabel = new JLabel("Ticket Info");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
-		add(lblNewLabel, "cell 0 15,alignx trailing");
-
-		JLabel lblDeliveryInfo_1 = new JLabel("Delivery Info");
-		lblDeliveryInfo_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		add(lblDeliveryInfo_1, "cell 2 15,alignx trailing");
-
-		JLabel lblPackageId = new JLabel("Package Id:");
-		add(lblPackageId, "cell 0 16,alignx trailing");
-
-		JLabel packageId = new JLabel("");
-		add(packageId, "cell 1 16");
-
-		JLabel lblCourier = new JLabel("Courier:");
-		add(lblCourier, "cell 2 16,alignx trailing");
-
-		JComboBox cbCourier = new JComboBox();
-		add(cbCourier, "cell 3 16,growx");
-
-		JLabel lblEstimatedDeliveryTime = new JLabel("Estimated Delivery Time:");
-		add(lblEstimatedDeliveryTime, "cell 0 17,alignx trailing");
-
-		JLabel estimatedDelivery = new JLabel("");
-		add(estimatedDelivery, "cell 1 17");
-
-		JLabel lblAssignedTime = new JLabel("Assigned Time:");
-		add(lblAssignedTime, "cell 2 17,alignx trailing");
-
-		JLabel departureTime = new JLabel("");
-		add(departureTime, "cell 3 17");
-
-		JLabel lblEstimatedBlocks = new JLabel("Estimated Blocks:");
-		add(lblEstimatedBlocks, "cell 0 18,alignx trailing");
-
-		JLabel blocks = new JLabel("");
-		add(blocks, "cell 1 18");
-
-		JLabel lblPickedUpTime = new JLabel("Picked Up Time:");
-		add(lblPickedUpTime, "cell 2 18,alignx trailing");
-
-		JLabel lblQuotedPrice = new JLabel("Quoted Price:");
-		add(lblQuotedPrice, "cell 0 19,alignx trailing");
-
-		JLabel price = new JLabel("");
-		add(price, "cell 1 19");
-
-		JLabel lblDeliveredTime = new JLabel("Delivered Time?");
-		add(lblDeliveredTime, "cell 2 19,alignx trailing");
-
-		JLabel lblBonus = new JLabel("Bonus?");
-		add(lblBonus, "cell 2 20,alignx trailing");
-
+		setVisible(true);
 	}
 }
