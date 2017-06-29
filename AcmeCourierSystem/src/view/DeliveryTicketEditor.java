c800ae1088b44c60d7b68c0f42a0d8dabb6cc8f5
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2017-06-28 19:20:06 -0500

    fixing saving of actual pickup and delivery times

--- AcmeCourierSystem/src/view/DeliveryTicketEditor.java
+++ AcmeCourierSystem/src/view/DeliveryTicketEditor.java
@@ -220,10 +220,10 @@ public class DeliveryTicketEditor extends JDialog {
 		price.setText(Double.toString(delivery.totalDeliveryCost));
 
 		delivery.assignedCourier = (Employee) cbCourier.getSelectedItem();
-		delivery.actualPickupTime = (delivery.actualPickupTime != null) ? LocalDateTime.of(delivery.actualPickupTime.toLocalDate(),
-				delivery.actualPickupTime.toLocalTime()) : null;
-		delivery.actualDeliveryTime = (delivery.actualDeliveryTime != null) ? LocalDateTime.of(delivery.actualDeliveryTime.toLocalDate(),
-				delivery.actualDeliveryTime.toLocalTime()) : null;
+		delivery.actualPickupTime = (pickedUpTime.getTime() != null)
+				? LocalDateTime.of(delivery.requestedPickupTime.toLocalDate(), pickedUpTime.getTime()) : null;
+		delivery.actualDeliveryTime = (deliveredTime.getTime() != null)
+				? LocalDateTime.of(delivery.requestedPickupTime.toLocalDate(), deliveredTime.getTime()) : null;
 		delivery.calculateBonus();
 		bonusEarned.setSelected(delivery.bonusEarned);
 
